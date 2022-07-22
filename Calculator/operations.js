19e1ffc150d1567f5cc992ae28ad15a857f25d3c
hlxie22 <68752252+hlxie22@users.noreply.github.com>
2022-07-21 22:54:28 -0500

    Create operations.js

---
+++ Calculator/operations.js
@@ -0,0 +1,8 @@
+const operate = (operator, a, b) => operator(a, b);
+
+// Basic operations
+
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
